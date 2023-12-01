f650a725122ab7d52c2e9500ef82fe647ff5ce55
mxygem <gh@nes.33mail.com>
2023-12-01 16:15:46 -0700

    3.2 JSON Encoding

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	if err := writeJSON(w, http.StatusOK, data, nil); err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w,
+			"The server encountered a problem and could not process your request",
+			http.StatusInternalServerError,
+		)
+	}
 }
