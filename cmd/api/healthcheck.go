18d97c042a0d8126b214ccf5f81ff0c9422a90c9
mxygem <gh@nes.33mail.com>
2023-12-01 17:07:31 -0700

    3.4 Formatting and Enveloping Responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,10 +5,12 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
 	if err := app.writeJSON(w, http.StatusOK, data, nil); err != nil {
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -28,7 +28,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		Version:   1,
 	}
 
-	if err := app.writeJSON(w, http.StatusOK, movie, nil); err != nil {
+	if err := app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil); err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 	}
