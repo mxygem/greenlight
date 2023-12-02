e27bc42f140ca5e9de5259ed0a67f42aa2288e34
mxygem <gh@nes.33mail.com>
2023-12-01 17:22:19 -0700

    3.6 Sending Error Messages

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -14,10 +14,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	if err := app.writeJSON(w, http.StatusOK, data, nil); err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w,
-			"The server encountered a problem and could not process your request",
-			http.StatusInternalServerError,
-		)
+		app.serverErrorResponse(w, r, err)
 	}
 }
