19514687873079efa6ae25151b29ab0bfcd07378
mxygem <gh@nes.33mail.com>
2023-12-23 20:51:34 -0700

    15.3 Authenticating Requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/mxygem/greenlight/internal/data"
+	"github.com/mxygem/greenlight/internal/validator"
 	"golang.org/x/time/rate"
 )
 
@@ -76,6 +80,64 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 			mu.Unlock()
 
 		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Add the "Vary: Authorization" header to the response. This indicates to any
+		// caches that the response may vary based on the value of the Authorization
+		// header in the request.
+		w.Header().Add("Vary", "Authorization")
+
+		// Retrieve the value of the Authorization header from the request. This will
+		// return the empty string "" if there is no such header found.
+		authorizationHeader := r.Header.Get("Authorization")
+
+		// If there is no Authorization header found, use the contextSetUser() helper
+		// to add the AnonymousUser to the request context. Then call the next handler
+		// in the chain and return without executing any of the code below.
+		if authorizationHeader == "" {
+			r = app.contextSetUser(r, data.AnonymousUser)
+			next.ServeHTTP(w, r)
+
+			return
+		}
+
+		// Otherwise, we expect the value of the Authorization header to be in the format
+		// "Bearer <token>". We try to split this into its constituent parts, and if the
+		// header isn't in the expected format we return a 401 Unauthorized response
+		// using the invalidAuthenticationTokenResponse() helper.
+		headerParts := strings.Split(authorizationHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		token := headerParts[1]
+
+		v := validator.New()
+
+		if data.ValidateTokenPlaintext(v, token); !v.Valid() {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		user, err := app.models.Users.GetForToken(data.ScopeAuthentication, token)
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidAuthenticationTokenResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, fmt.Errorf("unexpected error received looking up user by token: %w", err))
+			}
+
+			return
+		}
+
+		r = app.contextSetUser(r, user)
 
 		next.ServeHTTP(w, r)
 	})
